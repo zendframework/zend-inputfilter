d94fbfc089bbbf0c54983a044a3730550799dfb4
Marco Pivetta <ocramius@gmail.com>
2014-08-12 14:02:08 +0200

    Importing state as of zendframework/zf2@2ad4978b23a1d91cbbe5859f7b86cf4f50d54f3e (1393161974)
    Automatic import via rsync

--- Factory.php
+++ Factory.php
@@ -225,9 +225,6 @@ class Factory
                 case 'fallback_value':
                     $input->setFallbackValue($value);
                     break;
-                case 'break_on_failure':
-                    $input->setBreakOnFailure($value);
-                    break;
                 case 'filters':
                     if ($value instanceof FilterChain) {
                         $input->setFilterChain($value);
@@ -302,9 +299,6 @@ class Factory
             }
             if (isset($inputFilterSpecification['count'])) {
                 $inputFilter->setCount($inputFilterSpecification['count']);
-            }
-            if (isset($inputFilterSpecification['required'])) {
-                $inputFilter->setIsRequired($inputFilterSpecification['required']);
             }
             return $inputFilter;
         }
