7756cc1a6e312178f95fedbc75d19c277601ebe0
Gianluca Arbezzano <gianarb92@gmail.com>
2015-12-08 22:23:13 +0000

    ServiceManager v2-v3 compatibility

--- test/InputFilterAbstractServiceFactoryTest.php
+++ test/InputFilterAbstractServiceFactoryTest.php
@@ -36,8 +36,7 @@ class InputFilterAbstractServiceFactoryTest extends TestCase
     public function setUp()
     {
         $this->services = new ServiceManager();
-        $this->filters  = new InputFilterPluginManager();
-        $this->filters->setServiceLocator($this->services);
+        $this->filters  = new InputFilterPluginManager($this->services);
         $this->services->setService('InputFilterManager', $this->filters);
 
         $this->factory = new InputFilterAbstractServiceFactory();
@@ -45,42 +44,53 @@ class InputFilterAbstractServiceFactoryTest extends TestCase
 
     public function testCannotCreateServiceIfNoConfigServicePresent()
     {
-        $this->assertFalse($this->factory->canCreateServiceWithName($this->filters, 'filter', 'filter'));
+        $this->assertFalse($this->factory->canCreate($this->getCompatContainer(), 'filter'));
+        // v2
+        $this->assertFalse($this->factory->canCreateServiceWithName($this->getCompatContainer(), 'filter', 'filter'));
     }
 
     public function testCannotCreateServiceIfConfigServiceDoesNotHaveInputFiltersConfiguration()
     {
-        $this->services->setService('Config', []);
-        $this->assertFalse($this->factory->canCreateServiceWithName($this->filters, 'filter', 'filter'));
+        $this->services->setService('config', []);
+        $this->assertFalse($this->factory->canCreate($this->getCompatContainer(), 'filter'));
+        // v2
+        $this->assertFalse($this->factory->canCreateServiceWithName($this->getCompatContainer(), 'filter', 'filter'));
     }
 
     public function testCannotCreateServiceIfConfigInputFiltersDoesNotContainMatchingServiceName()
     {
-        $this->services->setService('Config', [
+        $this->services->setService('config', [
             'input_filter_specs' => [],
         ]);
-        $this->assertFalse($this->factory->canCreateServiceWithName($this->filters, 'filter', 'filter'));
+        $this->assertFalse($this->factory->canCreate($this->getCompatContainer(), 'filter'));
+        // v2
+        $this->assertFalse($this->factory->canCreateServiceWithName($this->getCompatContainer(), 'filter', 'filter'));
     }
 
     public function testCanCreateServiceIfConfigInputFiltersContainsMatchingServiceName()
     {
-        $this->services->setService('Config', [
+        $this->services->setService('config', [
             'input_filter_specs' => [
                 'filter' => [],
             ],
         ]);
-        $this->assertTrue($this->factory->canCreateServiceWithName($this->filters, 'filter', 'filter'));
+        $this->assertTrue($this->factory->canCreate($this->getCompatContainer(), 'filter'));
+        // v2
+        $this->assertTrue($this->factory->canCreateServiceWithName($this->getCompatContainer(), 'filter', 'filter'));
     }
 
     public function testCreatesInputFilterInstance()
     {
-        $this->services->setService('Config', [
+        $this->services->setService('config', [
             'input_filter_specs' => [
                 'filter' => [],
             ],
         ]);
-        $filter = $this->factory->createServiceWithName($this->filters, 'filter', 'filter');
+        $filter = $this->factory->__invoke($this->getCompatContainer(), 'filter');
         $this->assertInstanceOf(InputFilterInterface::class, $filter);
+        // v2
+        $v2filter = $this->factory->createServiceWithName($this->getCompatContainer(), 'filter', 'filter');
+        $this->assertEquals($filter, $v2filter);
     }
 
     /**
@@ -88,19 +98,19 @@ class InputFilterAbstractServiceFactoryTest extends TestCase
      */
     public function testUsesConfiguredValidationAndFilterManagerServicesWhenCreatingInputFilter()
     {
-        $filters = new FilterPluginManager();
+        $filters = new FilterPluginManager($this->services);
         $filter  = function ($value) {
         };
         $filters->setService('foo', $filter);
 
-        $validators = new ValidatorPluginManager();
+        $validators = new ValidatorPluginManager($this->services);
         /** @var ValidatorInterface|MockObject $validator */
         $validator  = $this->getMock(ValidatorInterface::class);
         $validators->setService('foo', $validator);
 
         $this->services->setService('FilterManager', $filters);
         $this->services->setService('ValidatorManager', $validators);
-        $this->services->setService('Config', [
+        $this->services->setService('config', [
             'input_filter_specs' => [
                 'filter' => [
                     'input' => [
@@ -117,8 +127,12 @@ class InputFilterAbstractServiceFactoryTest extends TestCase
             ],
         ]);
 
-        $inputFilter = $this->factory->createServiceWithName($this->filters, 'filter', 'filter');
+
+        $inputFilter = $this->factory->__invoke($this->getCompatContainer(), 'filter');
         $this->assertTrue($inputFilter->has('input'));
+        // v2
+        $v2InputFilter = $this->factory->createServiceWithName($this->getCompatContainer(), 'filter', 'filter');
+        $this->assertEquals($inputFilter, $v2InputFilter);
 
         $input = $inputFilter->get('input');
 
@@ -128,7 +142,7 @@ class InputFilterAbstractServiceFactoryTest extends TestCase
         $this->assertSame($filter, $filterChain->plugin('foo'));
         $this->assertEquals(1, count($filterChain));
 
-        $validatorChain = $input->getvalidatorChain();
+        $validatorChain = $input->getValidatorChain();
         $this->assertSame($validators, $validatorChain->getPluginManager());
         $this->assertEquals(1, count($validatorChain));
         $this->assertSame($validator, $validatorChain->plugin('foo'));
@@ -137,19 +151,7 @@ class InputFilterAbstractServiceFactoryTest extends TestCase
 
     public function testRetrieveInputFilterFromInputFilterPluginManager()
     {
-        $filters = new FilterPluginManager();
-        $filter  = function ($value) {
-        };
-        $filters->setService('foo', $filter);
-
-        $validators = new ValidatorPluginManager();
-        /** @var ValidatorInterface|MockObject $validator */
-        $validator  = $this->getMock(ValidatorInterface::class);
-        $validators->setService('foo', $validator);
-
-        $this->services->setService('FilterManager', $filters);
-        $this->services->setService('ValidatorManager', $validators);
-        $this->services->setService('Config', [
+        $this->services->setService('config', [
             'input_filter_specs' => [
                 'foobar' => [
                     'input' => [
@@ -165,9 +167,37 @@ class InputFilterAbstractServiceFactoryTest extends TestCase
                 ],
             ],
         ]);
+        $validators = new ValidatorPluginManager($this->services);
+        /** @var ValidatorInterface|MockObject $validator */
+        $validator  = $this->getMock(ValidatorInterface::class);
+        $this->services->setService('ValidatorManager', $validators);
+        $validators->setService('foo', $validator);
+
+        $filters = new FilterPluginManager($this->services);
+        $filter  = function ($value) {
+        };
+        $filters->setService('foo', $filter);
+
+        $this->services->setService('FilterManager', $filters);
         $this->services->get('InputFilterManager')->addAbstractFactory(InputFilterAbstractServiceFactory::class);
 
         $inputFilter = $this->services->get('InputFilterManager')->get('foobar');
         $this->assertInstanceOf(InputFilterInterface::class, $inputFilter);
+    }
+
+    /**
+     * Returns appropriate instance to pass to `canCreate()` et al depending on SM version
+     *
+     * v3 passes the 'creationContext' (ie the root SM) to the AbstractFactory, whereas v2 passes the PluginManager
+     */
+    protected function getCompatContainer()
+    {
+        if (method_exists($this->services, 'configure')) {
+            // v3
+            return $this->services;
+        } else {
+            // v2
+            return $this->filters;
+        }
     }
 }
