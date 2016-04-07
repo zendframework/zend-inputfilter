81af86fa49744fc19a32ae74dd263b2b8c1b24f8
Matthew Weier O'Phinney <matthew@zend.com>
2016-04-07 09:33:59 -0500

    Expose zend-inputfilter as a config-provider / component
    Adds:
    
    - `ConfigProvider`, which maps the `InputFilterPluginManager` to its factory,
      and enables the `InputFilterAbstractServiceFactory`.
    - `Module`, which does the above, and also provides an `init()` method for
      notifying the zend-modulemanager `ServiceListener` of the `input_filters`
      specification for aggregating input filter configuration.

---
+++ src/Module.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/zend-inputfilter for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Zend\InputFilter;
+
+class Module
+{
+    /**
+     * Return default zend-inputfilter configuration for zend-mvc applications.
+     */
+    public function getConfig()
+    {
+        $provider = new ConfigProvider();
+
+        return [
+            'service_manager' => $provider->getDependencyConfig(),
+        ];
+    }
+
+    /**
+     * Register a specification for the InputFilterManager with the ServiceListener.
+     *
+     * @param \Zend\ModuleManager\ModuleEvent
+     * @return void
+     */
+    public function init($event)
+    {
+        $container = $event->getParam('ServiceManager');
+        $serviceListener = $container->get('ServiceListener');
+
+        $serviceListener->addServiceManager(
+            'InputFilterManager',
+            'input_filters',
+            'Zend\ModuleManager\Feature\InputFilterProviderInterface',
+            'getInputFilterConfig'
+        );
+    }
+}
