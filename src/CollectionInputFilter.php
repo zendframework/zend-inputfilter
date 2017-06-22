fa01f9b2a7e86524bdf0711716dfaf1fe9dd0574
Filippo Tessarotto <zoeslam@gmail.com>
2017-06-22 15:21:17 +0200

    Prepare to PHP 7.2

--- src/CollectionInputFilter.php
+++ src/CollectionInputFilter.php
@@ -167,7 +167,7 @@ class CollectionInputFilter extends InputFilter
     /**
      * {@inheritdoc}
      */
-    public function isValid()
+    public function isValid($context = null)
     {
         $inputFilter = $this->getInputFilter();
         $valid = true;
