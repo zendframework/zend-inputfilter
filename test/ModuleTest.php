8a4c7b32a5de12e9271497f259d5c29d9b606ccc
Frank Brückner <dev@froschdesignstudio.de>
2019-05-09 13:02:39 +0200

    Updates links to licence in Doc-Blocks

--- test/ModuleTest.php
+++ test/ModuleTest.php
@@ -2,7 +2,7 @@
 /**
  * @link      https://github.com/zendframework/zend-inputfilter for the canonical source repository
  * @copyright Copyright (c) 2019 Zend Technologies USA Inc. (https://www.zend.com)
- * @license   https://framework.zend.com/license New BSD License
+ * @license   https://github.com/zendframework/zend-inputfilter/blob/master/LICENSE.md
  */
 
 namespace ZendTest\InputFilter;
